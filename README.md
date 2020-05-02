570b4cfb632925662aaaf004bf0484fd134668bc
Hayden Rouille <hayden@rouille.dev>
2020-05-01 21:26:52 -0400

    Write setup script to work with ubuntu. Add README details

--- README.md
+++ README.md
@@ -1,9 +1,72 @@
-This is where I keep my configuration files.
+# dotfiles
+This repository contains configuration files and setup for my current setup.
 
-What needs to be done when setting up vim and the whole environment is described in `dotenv.sh`.
+## What and why
+You may want to use this setup if you are interested in or use any of the below features.
 
-To run this, make sure you make it executable before you run it: (FYI I haven't testing it yet, it serves as a form of documentation for the moment)
-`chmod +x dotenv.sh && sh ./dotenv.sh`
+### Installed software
+- [asdf-vm](https://github.com/asdf-vm/asdf)
+- tmux with [tpm](https://github.com/tmux-plugins/tpm) and [tmux themepack](https://github.com/jimeh/tmux-themepack)
+- [neofetch](https://github.com/dylanaraps/neofetch)
+- [ripgrep](https://github.com/BurntSushi/ripgrep)
+- [fzf](https://github.com/junegunn/fzf)
+- [nnn](https://github.com/jarun/nnn)
+- mysql
+- git
+- [Ruby](https://github.com/ruby/ruby)
+- [Elixir](https://elixir-lang.org/)
+- yarn
 
-Please note: some of the configurations here expect this project to be nested as per the following `~/projects/dotfiles`
+## Vim
 
+![Dotfiles Demo](https://imgur.com/a/SqvUDvi)
+
+### Included plugins
+- [vim-plug](https://github.com/junegunn/vim-plug)
+- [vim-fugitive](https://github.com/tpope/vim-fugitive)
+- [nerdtree](https://github.com/preservim/nerdtree)
+- [gruvbox theme](https://github.com/morhetz/gruvbox)
+- [YouCompleteMe (Awesome JS completion)](https://github.com/ycm-core/YouCompleteMe)
+- [coc.nvim (Used for Ruby completion with Solargraph LSP)](https://github.com/neoclide/coc.nvim)
+- [undotree](https://github.com/mbbill/undotree)
+- [ale](https://github.com/dense-analysis/ale)
+- [vim-commentary](https://github.com/tpope/vim-commentary)
+- [vim-javascript](https://github.com/pangloss/vim-javascript)
+- [vim-markdown](https://github.com/plasticboy/vim-markdown)
+- [emmet-vim](https://github.com/mattn/emmet-vim)
+- [vim-easy-align](https://github.com/junegunn/vim-easy-align)
+- [vim-easymotion](https://github.com/easymotion/vim-easymotion)
+- [vim-system-copy](https://github.com/christoomey/vim-system-copy)
+- [vim-surround](https://github.com/tpope/vim-surround)
+- [auto-pairs](https://github.com/jiangmiao/auto-pairs)
+- [fzf.vim](https://github.com/junegunn/fzf.vim)
+
+### General usage
+Most of the key binds are self explanatory within the vimrc but here are some useful ones
+- `<Leader>` key is bound to `,`
+- `<Leader>sv` will vertically split
+- `<Leader>sx` will horizontally split
+- `<Leader>hjkl` for moving between splits
+- `<Leader>+` and `<Leader>-` for vertical resizing
+- `ga` for easy align. For instance, `gaip:` will align the paragraph on `:`
+- `<Leader><Enter>` to get rid of search highlighting
+- `<Leader>gd` will go to definition using YouCompleteMe or Coc.nvim depending on the filetype
+- `\` will trigger a grep from the cwd
+- `<Leader>nn` will toggle NerdTree
+- `<Leader>nf` will open NerdTree at the current files location
+- `<Leader>u` will toggle Undotree
+- `<C-T>` will open your file search
+- `<C-P>` same as above but with a preview of the files
+- `<Leader>b` opens a FZF file search for the open buffers
+- `<Leader>s` opens a search of the current files lines and will take you to the line found
+- `cp$` copy rest of line (or any pattern) to clipboard
+
+## Installation
+### Prerequisites
+If you want to use the setup script, then you should be on Ubuntu. Otherwise just reference it for installing your setup.
+
+```sh
+$ curl https://raw.githubusercontent.com/haydenrou/dotfiles/master/setup.sh >> ~/setup.sh
+$ chmod +x ~/setup.sh
+$ sh ~/setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+
+cd ~ && sudo apt-get update -y && sudo apt-get upgrade -y;
+
+echo "Installing the basics: git, vim, tmux, wget, ruby, rubygems, vim-nox, fzf, ripgrep"
+sudo apt-get install -y \
+  git \
+  vim \
+  tmux \
+  wget \
+  gpg \
+  dirmngr
+  nnn \
+  xsel \
+  curl \
+  ruby \
+  rubygems \
+  vim-nox \
+  neofetch \
+  silversearcher-ag \
+  ripgrep \
+  powerline \
+  libc6 \
+  fzf \
+  libmysqlclient-dev \
+  mysql-server \
+  mysql-client
+
+echo "As per https://github.com/junegunn/fzf/issues/1589 we'll have to install fzf manually on Ubuntu 18.04 at the moment";
+read -p "Are you running Ubuntu? [Y/n]? " ubuntu
+case $ubuntu in
+  [Yy]* ) wget http://mirrors.kernel.org/ubuntu/pool/universe/f/fzf/fzf_0.18.0-2_amd64.deb | sudo dkpg -i;;
+  [Nn]* ) exit;;
+  * ) exit;;
+esac
+ls -a ~ | grep fzf_ | xargs rm
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+read -p "What email do you want associated with your git config? " gitemail
+git config --global user.email "$gitemail"
+read -p "And the name? " gitname
+git config --global user.name "$gitname"
+
+mkdir ~/projects
+mkdir ~/.vim/undodir -p
+
+mkdir ~/.fonts
+wget https://github.com/abertsch/Menlo-for-Powerline/raw/master/Menlo%20for%20Powerline.ttf -P ~/.fonts/
+
+echo "Making an SSH Key";
+ssh-keygen -t rsa
+eval `ssh-agent`
+ssh-add ~/.ssh/id_rsa
+
+echo $(cat ~/.ssh/id_rsa.pub)
+read -p "Press <Enter> once you have added the SSH key above to github.";
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.8
+
+git clone git@github.com:haydenrou/dotfiles.git ~/projects/dotfiles
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+mkdir ~/.config/solargraph -p
+rm ~/.bashrc ~/.bash_profile ~/.tmux.conf ~/.vimrc > /dev/null 2>&1
+ln -s ~/projects/dotfiles/aginore.symlink ~/.agignore
+ln -s ~/projects/dotfiles/alacritty.symlink.yml ~/.alacritty.yml
+ln -s ~/projects/dotfiles/profile.symlink ~/.profile
+ln -s ~/projects/dotfiles/bash_profile.symlink ~/.bash_profile
+ln -s ~/projects/dotfiles/bashrc.symlink ~/.bashrc
+ln -s ~/projects/dotfiles/tmux.conf.symlink ~/.tmux.conf
+ln -s ~/projects/dotfiles/vimrc.symlink ~/.vimrc
+ln -s ~/projects/dotfiles/solargraph.config.yml ~/.config/solargraph/config.yml
+
+echo "and install your tmux plugins :)";
+~/.tmux/plugins/tpm/bin/install_plugins
+
+read -p "Would you like to install NodeJS? [Y/n]" nodejs
+case $nodejs in
+  [Yy]* )
+    asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+    bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+    read -p "Enter a specific version (the LTS at time of writing is 12.16.3) " nodejs_version
+    asdf install nodejs $nodejs_version
+    asdf global nodejs $nodejs_version
+    ;;
+  [Nn]* ) exit;;
+  * ) exit;;
+esac
+
+sudo apt-get install -y
+  nodejs \
+  gcc \
+  g++ \
+  make
+
+echo "This is going to configure YCM for vim";
+sudo apt-get install -y cmake python-dev python3-dev build-essential;
+
+mkdir ~/ycm_build && cd ~/ycm_build
+
+cmake -G "Unix Makefiles" . ~/.vim/plugged/YouCompleteMe/third_party/ycmd/cpp && cmake --build . --target ycm_core --config Release
+
+cd ~/.vim/plugged/YouCompleteMe
+./install.py --ts-completer
+
+source ~/.profile
+
+vim +Plug Install +qall
+vim +YcmRestartServer +qall
+vim -c "call coc#util#install()|qall"
+vim -c "CocInstall coc-solargraph|echo 'When coc-solargraph is installed it will let you know, and you can exit with :qa'"
+
+read -p "Would you like to install Yarn? [Y/n]" yarn
+case $yarn in
+  [Yy]* )
+    asdf plugin-add yarn
+    read -p "Enter a specific version (the LTS at time of writing is 1.17.3) " yarn_version
+    asdf install yarn $yarn_version
+    asdf global yarn $yarn_version
+    ;;
+  [Nn]* ) exit;;
+  * ) exit;;
+esac
+
+read -p "Do you want to install Elixir? [Y/n] " elixir
+case $elixir in
+  [Yy]* ) wget https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb && sudo dpkg -i erlang-solutions_2.0_all.deb && sudo apt-get update -y && sudo apt-get install esl-erlang -y && sudo apt-get install elixir -y;;
+  [Nn]* ) exit;;
+  * ) exit;;
+esac
+
+read -p "Do you want to install Ruby? [Y/n] " ruby
+case $ruby in
+  [Yy]* )
+    asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+    read -p "Enter a specific version  " ruby_version
+    asdf install ruby $ruby_version
+    asdf global ruby $ruby_version
+    gem install solargraph
+    gem install mailcatcher
+    ;;
+  [Nn]* ) exit;;
+  * ) exit;;
+esac
+
+read -p "Do you want the mysql2 gem at version 0.5.2? [Y/n] " versioned_mysql
+case $versioned_mysql in
+  [Yy]* )
+    gem install mysql2 -v "0.5.2"
+    ;;
+  [Nn]* ) exit;;
+  * ) exit;;
+esac
+
+echo "Moving onto database setup:"
+read -p "What password do you want _root_ to be set as for mysql?" password
+
+PASSWORD_SCRIPT="use mysql; update user set authentication_string=PASSWORD(\"${password}\") where User='root'; update user set plugin=\"mysql_native_password\" where User='root'; flush privileges; quit;"
+
+sudo /etc/init.d/mysql stop && sudo mysqld_safe --skip-grant-tables && mysql -uroot < PASSWORD_SCRIPT && sudo /etc/init.d/mysql stop && sudo /etc/init.d/mysql start
+
+echo '==========================================================================='
+echo "Done! You can now `rm ~/setup.sh`, and there is a copy in `~/projects/dotfiles` for reference if you're interested. Please read the README to see further details. Enjoy..."
+echo '==========================================================================='
+
