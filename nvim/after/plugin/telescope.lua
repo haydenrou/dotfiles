2ba84e6d73ed7ccc1b04da951725eafb1feab286
Hayden Rouille <hayden@rouille.net>
2023-12-24 17:13:09 -0500

    Add fzf for telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,5 +1,17 @@
 local builtin = require('telescope.builtin')
+local telescope = require('telescope')
+
+telescope.load_extension('fzf')
+
+vim.keymap.set('n', '\\', function()
+  builtin.grep_string({
+    path_display = { 'smart' },
+    only_sort_text = true,
+    word_match = "-w",
+    search = '',
+  })
+end)
+
 vim.keymap.set('n', '<C-t>', builtin.git_files, {})
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '\\', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
--- nvim/lua/talakhadze/lazy.lua
+++ nvim/lua/talakhadze/lazy.lua
@@ -14,9 +14,10 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-    { 'nvim-telescope/telescope.nvim',    tag = '0.1.5',      dependencies = { 'nvim-lua/plenary.nvim' } },
+    { 'nvim-telescope/telescope.nvim',            tag = '0.1.5',                                                                                                                         dependencies = { 'nvim-lua/plenary.nvim' } },
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
 
-    { "nvim-treesitter/nvim-treesitter",  build = ":TSUpdate" },
+    { "nvim-treesitter/nvim-treesitter",          build = ":TSUpdate" },
 
     -- Colorscheme
     { "savq/melange-nvim" },
@@ -24,13 +25,13 @@ local plugins = {
     -- lsp-zero
     { 'williamboman/mason.nvim' },
     { 'williamboman/mason-lspconfig.nvim' },
-    { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
+    { 'VonHeikemen/lsp-zero.nvim',                branch = 'v3.x' },
     { 'neovim/nvim-lspconfig' },
     { 'hrsh7th/cmp-nvim-lsp' },
     { 'hrsh7th/nvim-cmp' },
     { 'L3MON4D3/LuaSnip' },
 
-    { 'ThePrimeagen/harpoon',             branch = 'harpoon2' },
+    { 'ThePrimeagen/harpoon',                     branch = 'harpoon2' },
 
     { 'mbbill/undotree' },
 
