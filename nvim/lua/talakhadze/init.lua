a706e14c2d542507636b2d96931c2aaf6dc4611e
Hayden Rouille <hayden@rouille.net>
2024-01-06 10:44:21 -0500

    Remove comment and add help tags binding for telescope

--- nvim/lua/talakhadze/init.lua
+++ nvim/lua/talakhadze/init.lua
@@ -1,9 +1,3 @@
--- ============================================================================== --
---  FILE:         init.lua                                                        --
---  AUTHOR:       Hayden Rouille :: https://github.com/haydenrou                  --
---  DESCRIPTION:  NeoVim configuration file                                       --
--- ============================================================================== --
-
 vim.g.mapleader = " "
 
 require("talakhadze.lazy")
