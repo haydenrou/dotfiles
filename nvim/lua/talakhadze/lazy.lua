32d293ed83e93a17cf0b1ce2a340196945405951
Hayden Rouille <hayden@rouille.net>
2024-03-02 11:46:10 +0000

    Remove unused buffer writes that are set up from conform

--- nvim/lua/talakhadze/lazy.lua
+++ nvim/lua/talakhadze/lazy.lua
@@ -14,26 +14,26 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
-    { 'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' } },
+    { 'nvim-telescope/telescope-fzf-native.nvim',   build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
+    { 'nvim-telescope/telescope.nvim',              tag = '0.1.5',                                                                                                                         dependencies = { 'nvim-lua/plenary.nvim' } },
 
     { "JoosepAlviste/nvim-ts-context-commentstring" },
 
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate", dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" } },
+    { "nvim-treesitter/nvim-treesitter",            build = ":TSUpdate",                                                                                                                   dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" } },
 
     -- Colour scheme
-    { "rose-pine/neovim", lazy = false, priority = 1000 },
+    { "rose-pine/neovim",                           lazy = false,                                                                                                                          priority = 1000 },
 
     -- lsp-zero
     { 'williamboman/mason.nvim' },
     { 'williamboman/mason-lspconfig.nvim' },
-    { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' },
+    { 'VonHeikemen/lsp-zero.nvim',                  branch = 'v3.x' },
     { 'neovim/nvim-lspconfig' },
     { 'hrsh7th/cmp-nvim-lsp' },
     { 'hrsh7th/nvim-cmp' },
     { 'L3MON4D3/LuaSnip' },
 
-    { 'ThePrimeagen/harpoon', branch = 'harpoon2' },
+    { 'ThePrimeagen/harpoon',                       branch = 'harpoon2' },
 
     { 'mbbill/undotree' },
 
@@ -48,11 +48,11 @@ local plugins = {
     { 'nvim-tree/nvim-web-devicons' },
 
     { 'nvim-lualine/lualine.nvim' },
-    { 'numToStr/Comment.nvim', dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" } },
+    { 'numToStr/Comment.nvim',                      dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" } },
 
-    { "kylechui/nvim-surround", version = "*", event = "VeryLazy" },
+    { "kylechui/nvim-surround",                     version = "*",                                                                                                                         event = "VeryLazy" },
 
-    { 'goolord/alpha-nvim', dependencies = { "nvim-tree/nvim-web-devicons" }, event = "VimEnter" }
+    { 'goolord/alpha-nvim',                         dependencies = { "nvim-tree/nvim-web-devicons" },                                                                                      event = "VimEnter" }
 }
 
 require("lazy").setup(plugins, opts)
