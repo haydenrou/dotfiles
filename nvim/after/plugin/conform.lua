bdcdc0ed08578369cf2a95d10dcf703a74d55015
Hayden Rouille <hayden@rouille.net>
2024-03-02 11:48:28 +0000

    Add conform config

---
+++ nvim/after/plugin/conform.lua
@@ -0,0 +1,16 @@
+local conform = require("conform")
+
+conform.setup({
+    formatters_by_ft = {
+        lua = { "stylua" },
+        go = { "goimports", "gofmt" },
+        templ = { "rustywind", "templ" },
+        javascript = { "eslint", { "prettierd", "prettier" } },
+        typescript = { { "eslint" }, { "prettierd", "prettier" } },
+        ["_"] = { "trim_whitespace" },
+    },
+    format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
+})
