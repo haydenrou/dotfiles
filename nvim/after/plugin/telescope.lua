1748efbc2e523d5b5e3a8ad31826eacbf01ba6c0
Hayden Rouille <hayden@rouille.dev>
2024-10-24 12:52:14 +0100

    Update telescope configuration

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,61 +1,38 @@
 local nmap = require("talakhadze.utils.maps").nmap
 
 local builtin = require('telescope.builtin')
-local telescope = require('telescope')
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
 
-telescope.setup({
-    pickers = {
-        colorscheme = { enable_preview = true },
-        find_files = {
-            find_command = { "fd", "--type", "f", "--hidden", "--ignore-file", ".gitignore", "--strip-cwd-prefix" },
-            theme = "ivy",
-        },
-        oldfiles = {
-            theme = "ivy",
-        },
-        live_grep = {
-            layout_strategy = "vertical",
-        },
-        buffers = { ignore_current_buffer = true },
-    },
-    defaults = {
-        theme = "ivy",
-        vimgrep_arguments = {
-            "rg",
-            "--no-heading",
-            "--with-filename",
-            "--line-number",
-            "--column",
-            "--smart-case",
-            "--hidden",
-            "--glob=!{.git,.svn,.hg,.DS_Store,thumbs.db,node_modules,bower_components}",
-        },
-        sort_mru = true,
-        sorting_strategy = "ascending",
-        color_devicons = true,
-        layout_strategy = "horizontal",
-        layout_config = {
-            prompt_position = "top",
-            horizontal = { width_padding = 0.04, height_padding = 0.1 },
-            vertical = { width_padding = 0.05, height_padding = 1 },
-        },
-        border = true,
-        prompt_prefix = "   ",
-        hl_result_eol = false,
-        wrap_results = true,
-        hidden = true,
-    },
-    extensions = {
-        fzf = {
-            fuzzy = true,
-            override_generic_sorter = true,
-            override_file_sorter = true,
-            case_mode = "smart_case",
-        },
-    },
-})
+-- https://github.com/nvim-telescope/telescope.nvim/issues/416#issuecomment-841273053
+local function fzf_multi_select(prompt_bufnr)
+  local picker = action_state.get_current_picker(prompt_bufnr)
+  local num_selections = #picker:get_multi_selection()
 
-telescope.load_extension('fzf')
+  if num_selections > 1 then
+    actions.send_selected_to_qflist(prompt_bufnr)
+    actions.open_qflist()
+  else
+    actions.file_edit(prompt_bufnr)
+  end
+end
+
+require("telescope").setup {
+  defaults = {
+    mappings = {
+      i = {
+        ["<tab>"] = actions.toggle_selection + actions.move_selection_previous,
+        ["<s-tab>"] = actions.toggle_selection + actions.move_selection_next,
+        ["<cr>"] = fzf_multi_select
+      },
+      n = {
+        ["<tab>"] = actions.toggle_selection + actions.move_selection_previous,
+        ["<s-tab>"] = actions.toggle_selection + actions.move_selection_next,
+        ["<cr>"] = fzf_multi_select
+      }
+    }
+  }
+}
 
 nmap('\\', builtin.live_grep, {})
 nmap('<C-t>', builtin.find_files, {})
